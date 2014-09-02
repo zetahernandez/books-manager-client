51263c1a0cb10d3e9eb14f60040a3a17bf692327
zetahernandez <zetahernandez@gmail.com>
2014-09-02 00:39:58 -0300

    chabnges

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,27 @@
-angular.module( 'books-manager', [
+'use strict';
+angular.module('books-manager', [
   'templates-app',
   'templates-common',
   'books-manager.home',
   'books-manager.about',
-  'ui.router'
+  'ui.router',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
 ])
 
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
+.config(function myAppConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
 })
 
-.run( function run () {
-})
+.run(function run() {})
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | books-manager' ;
+.controller('AppCtrl', function AppCtrl($scope, $location) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    if (angular.isDefined(toState.data.pageTitle)) {
+      $scope.pageTitle = toState.data.pageTitle + ' | books-manager';
     }
   });
 })
 
 ;
-
