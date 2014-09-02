d940cec167e31029b19a068469e63a3ba672a6da
zetahernandez <zetahernandez@gmail.com>
2014-09-01 23:27:11 -0300

    initial commit

---
+++ src/app/app.js
@@ -0,0 +1,25 @@
+angular.module( 'books-manager', [
+  'templates-app',
+  'templates-common',
+  'books-manager.home',
+  'books-manager.about',
+  'ui.router'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | books-manager' ;
+    }
+  });
+})
+
+;
+
